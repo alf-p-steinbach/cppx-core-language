51cdc55ce1e5d67ad2efb8a321c3b9bf290224d6
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-17 15:44:34 +0100

    Initial commit

---
+++ dummy-header-includes/tmp-support/basic-type-traits.include.cpp
@@ -0,0 +1,2 @@
+#include <cppx-core-language/tmp/type-traits.hpp>
+namespace { int satisfy_linker; }
---
+++ source/types/all.hpp
@@ -0,0 +1,6 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+
+#include <cppx-core-language/types/C_str_.hpp>
+#include <cppx-core-language/types/fixed-size-checking.hpp>
+#include <cppx-core-language/types/Int_.hpp>
+#include <cppx-core-language/types/Truth.hpp>
