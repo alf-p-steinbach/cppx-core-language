e4c7c242f33764729801d2b0cf7172551f1aa714
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-21 06:43:08 +0100

    ASCII support moved here from cppx-core library

---
+++ source/ascii/all.hpp
@@ -0,0 +1,5 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+
+#include <cppx-core-language/ascii/ascii-character-names.hpp>
+#include <cppx-core-language/ascii/ascii-util.hpp>
+#include <cppx-core-language/ascii/hex-digits.hpp>
