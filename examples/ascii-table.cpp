01564cecd4ff4175a16a711ab8b186ed5968d217
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-24 00:30:18 +0100

    -

--- examples/ascii-table.cpp
+++ examples/ascii-table.cpp
@@ -1,14 +1,13 @@
+#include <cppx-core-language/all.hpp>
 #include <iomanip>          // std::setw
 #include <iostream>
-#include <cppx-core-language/all.hpp>
 
-auto main() -> int
+auto main()
+    -> int
 {
-    // Instead of `using std::cout, std::endl, std::left, std::setw;`, just:
     $use_std( cout, endl, left, setw );
-
-    $use_cppx( zero_to, hex_digit );    // Use identifiers cppx::zero_to, cppx::hex_digit.
-    $use_cppx_nsname( ascii );          // Use namespace name cppx::ascii.
+    $use_cppx( zero_to, hex_digit );
+    $use_cppx_nsname( ascii );
     
     const auto field = setw( 4 );
     cout << left;
@@ -17,13 +16,13 @@ auto main() -> int
     cout << field << "";
     for( const int x: zero_to( 16 ) ) { cout << field << hex_digit( x ); }
     cout << endl;
-    cout << endl;   // Spacer line to make the header row stand out as such.
+    cout << endl;               // Spacer line to make the header row stand out as such.
     
     // Main table with a row header column to the left.
     for( const int y: zero_to( 8 ) ) {
         cout << field << hex_digit( y );
         for( const int x: zero_to( 16 ) ) {
-            const char code = 16*y + x;
+            const int code = 16*y + x;
             const auto ch = char( ascii::is_noncontrol_char( code )? code: ascii::del );
             cout << field << ch;
         }
---
+++ examples/ascii-table.stdlib.cpp
@@ -0,0 +1,37 @@
+#include <ctype.h>          // ::isprint
+#include <iomanip>          // std::setw
+#include <iostream>
+#include <limits.h>         // CHAR_MIN, UCHAR_MAX    
+
+using Byte = unsigned char;
+
+auto is_noncontrol_ascii_char( const int code )
+    -> bool
+{ return (code >= CHAR_MIN and code <= UCHAR_MAX and ::isprint( Byte( code ) ); }
+
+auto main()
+    -> int
+{
+    using std::cout, std::endl, std::left, std::setw;
+    constexpr auto& hex_digits = "0123456789abcdef";
+    
+    const auto field = setw( 4 );
+    cout << left;
+    
+    // Column headers.
+    cout << field << "";
+    for( int x = 0; x < 16; ++x ) { cout << field << hex_digits[x]; }
+    cout << endl;
+    cout << endl;               // Spacer line to make the header row stand out as such.
+    
+    // Main table with a row header column to the left.
+    for( int y = 0; y < 8; ++y ) {
+        cout << field << hex_digits[y];
+        for( int x = 0; x < 16; ++x ) {
+            const int code = 16*y + x;
+            const auto ch = char( is_noncontrol_ascii_char( code )? code: ascii::del );
+            cout << field << ch;
+        }
+        cout << endl;
+    }
+}
