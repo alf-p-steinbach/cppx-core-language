5fa3b7f90b86643f8c0133f1962f6e358a35eb3e
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-23 23:41:53 +0100

    Update ascii-table.cpp

--- examples/ascii-table.cpp
+++ examples/ascii-table.cpp
@@ -24,7 +24,7 @@ auto main() -> int
         cout << field << hex_digit( y );
         for( const int x: zero_to( 16 ) ) {
             const char code = 16*y + x;
-            const auto ch = (ascii::is_control_char( code )? ascii::del : code);
+            const auto ch = char( ascii::is_control_char( code )? ascii::del : code );
             cout << field << ch;
         }
         cout << endl;
