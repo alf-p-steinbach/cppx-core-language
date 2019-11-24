a437a7da7a229d1ef430efb8fa022ca353484558
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-24 06:47:21 +0100

    Create string-argument-formatting.cpp

---
+++ examples/tutorial/syntax/string-argument-formatting.cpp
@@ -0,0 +1,29 @@
+#include <cppx-core-language/all.hpp>
+#include <iostream>         // std::(cout, endl)
+#include <string>           // std::(to_string, operator ""s)
+#include <string_view>      // std::string_view
+
+namespace my{
+    $use_std( string_view, cout, endl );
+
+    void display( const string_view& s )
+    {
+        cout << s << endl;
+    }
+}  // namespace my
+
+auto main()
+    -> int
+{
+    using namespace cppx::basic_string_building;
+    $use_std( to_string );
+
+    const double    a   = 1.23;
+    const double    b   = 4.56;
+    
+    my::display( ""s
+        << "One would expect that " << a << " + " << b << " = " << a + b
+        << ", not " << to_string( a + b ) << "!"
+        );
+    my::display( "However, integer arithmetic is exact; 2 + 2 = "s << 2 + 2 << "." );
+}
