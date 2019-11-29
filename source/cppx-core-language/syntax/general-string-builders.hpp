a34c0e48e4606de437d9bc33475941438e5a1d53
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-29 03:54:16 +0100

    General string builders moved here from cppx-core project

---
+++ source/cppx-core-language/syntax/general-string-builders.hpp
@@ -0,0 +1,53 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+#include <cppx-core-language/syntax/macro-use.hpp>      // CPPX_USE_STD
+#include <cppx-core-language/syntax/Span_.hpp>          // cppx::Span_
+
+#include <string>           // std::string
+#include <string_view>      // std::string_view
+
+namespace cppx {
+    CPPX_USE_STD( string, string_view );
+
+    inline namespace basic_string_builders {
+
+        inline auto spaces( const int n )
+            -> string
+        { return (n <= 0? "" : string( n, ' ')); }
+
+        inline auto repeated_times( const int n, const string_view& s )
+            -> string
+        {
+            if( n <= 0 ) { return ""; }
+
+            string result;
+            result.reserve( n*s.length() );
+            for( int i = 1; i <= n; ++i )
+            {
+                result += s;
+            }
+            return result;
+        }
+
+        inline auto operator*( const int n, const string_view& s )
+            -> string
+        { return repeated_times( n, s ); }
+
+        template< class Iterator >      // TODO: Enable_if_
+        inline auto joined(
+            const Span_<Iterator>       range,
+            const string_view&          separator = " "
+            ) -> string
+        {
+            if( is_empty( range ) ) { return ""; }
+
+            string result = range.front();
+            for( const auto& item : all_but_first_of( range ) )
+            {
+                result += separator;
+                result += item;
+            }
+            return result;
+        }
+
+    }  // namespace basic_string_builders
+}  // namespace cppx
---
+++ source/dummy-header-includes/syntax/general-string-builders.include.cpp
@@ -0,0 +1,2 @@
+#include <cppx-core-language/syntax/general-string-builders.hpp>
+namespace{ int satisfy_linker; }
