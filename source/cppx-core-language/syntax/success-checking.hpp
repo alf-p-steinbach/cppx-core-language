915fc4e37335c2f2c88b7bb5ca77e34a41601818
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-29 04:55:38 +0100

    Update success-checking.hpp

--- source/cppx-core-language/syntax/success-checking.hpp
+++ source/cppx-core-language/syntax/success-checking.hpp
@@ -2,7 +2,7 @@
 
 #include <cppx-core-language/types/Truth.hpp>           // cppx::Truth
 
-namespace cppx{
+namespace cppx::success_checking {
 
     //------------------------------------------ Success, Failure & `>>`
     //
@@ -24,36 +24,35 @@ namespace cppx{
     //          -> Truth
     //      { return SUCCEEDED( hr ); }
 
-    namespace success_checking {
-        struct Success{};
-        struct Is_zero{};
+    struct Success{};
+    struct Is_zero{};
 
-        // The intent is that client code provides meanings for `expr >> Success`.
-        // Others can be expressed in terms of such client code provided meanings.
+    // The intent is that client code provides meanings for `expr >> Success`.
+    // Others can be expressed in terms of such client code provided meanings.
 
-        template< class Value >
-        auto operator>>( const Value& v, Is_zero )
-            -> Truth
-        { return v == 0; }
-    }  // namespace success_checking
+    template< class Value >
+    auto operator>>( const Value& v, Is_zero )
+        -> Truth
+    { return v == 0; }
+}  // namespace cppx::success_checking
 
+namespace cppx::failure_checking {
+    using namespace success_checking;
+
+    //--------------------------------------------- Inverted condition:
+    struct Failure{};
+
+    template< class Value >
+    auto operator>>( const Value& v, Failure )
+        -> Truth
+    { return not (v >> Success()); }
+}  // namespace cppx::failure_checking
+
+namespace cppx {
     using success_checking::Success;
     using success_checking::Is_zero;
     using success_checking::operator>>;
 
-
-    //--------------------------------------------- Inverted condition:
-
-    namespace failure_checking {
-        struct Failure{};
-
-        template< class Value >
-        auto operator>>( const Value& v, Failure )
-            -> Truth
-        { return not (v >> Success()); }
-    }  // namespace failure_checking
-
     using failure_checking::Failure;
     using failure_checking::operator>>;
-
 }  // namespace cppx
