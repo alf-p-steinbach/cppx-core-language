691bfadd5c1fe643a14b164f11531e24b4fd6c69
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-29 04:38:45 +0100

    Update basic-string-assembly.hpp

--- source/cppx-core-language/syntax/basic-string-assembly.hpp
+++ source/cppx-core-language/syntax/basic-string-assembly.hpp
@@ -7,73 +7,72 @@
 #include <string_view>      // std::string_view
 #include <sstream>          // std::ostringstream
 
-namespace cppx
+namespace cppx::basic_string_assembly
 {
     CPPX_USE_STD( ostringstream, string, string_view );
+    using namespace std::string_literals;       // E.g. ""s
 
-    namespace basic_string_assembly
+    // Quite inefficient but this is the general case fallback. Client code might specialize
+    // it for efficiency. That's not a concern here, except to support that it can be done.
+    template< class Type >
+    inline auto string_from( const Type& value )
+        -> string
     {
-        using namespace std::string_literals;       // E.g. ""s
+        ostringstream stream;
+        stream << value;
+        return stream.str();
+    }
 
-        // Quite inefficient but this is the general case fallback. Client code might specialize
-        // it for efficiency. That's not a concern here, except to support that it can be done.
-        template< class Type >
-        inline auto string_from( const Type& value )
-            -> string
-        {
-            ostringstream stream;
-            stream << value;
-            return stream.str();
-        }
+    template< class Type >
+    inline auto operator<<( string& s, const Type& value )
+        -> string&
+    {
+        s += string_from( value );
+        return s;
+    }
 
-        template< class Type >
-        inline auto operator<<( string& s, const Type& value )
-            -> string&
-        {
-            s += string_from( value );
-            return s;
-        }
+    template<>
+    inline auto operator<< <char>( string& s, const char& more )
+        -> string&
+    {
+        s += more;
+        return s;
+    }
 
-        template<>
-        inline auto operator<< <char>( string& s, const char& more )
-            -> string&
-        {
-            s += more;
-            return s;
-        }
+    template<>
+    inline auto operator<< <C_str>( string& s, const C_str& more )
+        -> string&
+    {
+        s += more;
+        return s;
+    }
 
-        template<>
-        inline auto operator<< <C_str>( string& s, const C_str& more )
-            -> string&
-        {
-            s += more;
-            return s;
-        }
+    template<>
+    inline auto operator<< <string_view>( string& s, const string_view& more )
+        -> string&
+    {
+        s += more;
+        return s;
+    }
 
-        template<>
-        inline auto operator<< <string_view>( string& s, const string_view& more )
-            -> string&
-        {
-            s += more;
-            return s;
-        }
+    template<>
+    inline auto operator<< <string>( string& s, const string& more )
+        -> string&
+    {
+        s += more;
+        return s;
+    }
 
-        template<>
-        inline auto operator<< <string>( string& s, const string& more )
-            -> string&
-        {
-            s += more;
-            return s;
-        }
+    template< class Type >
+    inline auto operator<<( string&& s, Type const& value )
+        -> string&&
+    { return move( operator<<( s, value ) ); }
 
-        template< class Type >
-        inline auto operator<<( string&& s, Type const& value )
-            -> string&&
-        { return move( operator<<( s, value ) ); }
+}  // namespace cppx::basic_string_assembly
 
-    }  // namespace basic_string_assembly
-
-    // Intentionally not exposing `basic_string_building::string_from` here.
+namespace cppx
+{
+        // Intentionally not exposing `basic_string_building::string_from` here.
     // It might interfere with other `string_from` functions.
     using namespace std::string_literals;       // E.g. ""s
     using basic_string_assembly::operator<<;
