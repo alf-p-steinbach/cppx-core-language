7783135fa526f0e1ddf75a18ae7540c56e32fb72
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-30 01:43:55 +0100

    Update type-traits.hpp

--- source/cppx-core-language/tmp/type-traits.hpp
+++ source/cppx-core-language/tmp/type-traits.hpp
@@ -16,8 +16,9 @@
 /// \make_name_ref{cppx,is_enum_},
 /// \make_name_ref{cppx,is_nullpointer_},
 /// \make_name_ref{cppx,is_pointer_},
-/// \make_name_ref{cppx,is_scalar_},
-/// \make_name_ref{cppx,is_a_basic_char_type_} and
+/// \make_name_ref{cppx,is_basic_value_},
+/// \make_name_ref{cppx,is_a_byte_char_type_},
+/// \make_name_ref{cppx,is_a_nonbyte_char_type_} and
 /// \make_name_ref{cppx,is_a_char_type_}.
 ///
 /// `is_based_and_derived_` clarifies the roles of the template arguments, which are not
@@ -45,15 +46,17 @@ namespace cppx
     constexpr Truth is_a_                   = is_same_type_<T, U> or is_base_and_derived_<T, U>;
 
     //----------------------------------------------------------------------------------------------
+    //
+    // Categories in a strict hierarchy of type categories.
 
     template< class T >
-    constexpr Truth is_trivially_copyable_  = std::is_trivially_copyable_v<T>;
-
-    template< class T >
-    constexpr Truth is_const_               = std::is_const_v<std::remove_reference_t<T>>;
+    constexpr Truth is_arithmetic_          = std::is_arithmetic_v<T>;
 
     template< class T >
     constexpr Truth is_class_               = std::is_class_v<T>;
+
+    template< class T >
+    constexpr Truth is_enum_                = std::is_enum_v<T>;
 
     template< class T >
     constexpr Truth is_fixed_point_         = false;
@@ -64,26 +67,34 @@ namespace cppx
     template< class T >
     constexpr Truth is_integral_            = std::is_integral_v<T>;
 
-    template< class Int >
-    constexpr Truth is_signed_              = std::is_signed_v<Int>;
-
-    template< class Int >
-    constexpr Truth is_unsigned_            = std::is_unsigned_v<Int>;
-
-    template< class T >
-    constexpr Truth is_arithmetic_          = std::is_arithmetic_v<T>;
-
-    template< class T >
-    constexpr Truth is_enum_                = std::is_enum_v<T>;
-
     template< class T >
     constexpr Truth is_nullpointer_         = std::is_null_pointer_v<T>;
 
     template< class T >
     constexpr Truth is_pointer_             = std::is_pointer_v<T>;
 
+    template< class Int >
+    constexpr Truth is_signed_              = std::is_signed_v<Int>;
+
+    template< class Int >
+    constexpr Truth is_unsigned_            = std::is_unsigned_v<Int>;
+
+    // Unfortunately a class like Truth can't in general be treated as a basic value.
     template< class T >
-    constexpr Truth is_scalar_              = std::is_scalar_v<T>;
+    constexpr Truth is_basic_value_         =
+        is_arithmetic_<T> or is_enum_<T> or is_pointer_<T>;
+
+    //----------------------------------------------------------------------------------------------
+
+    template< class T >
+    constexpr Truth is_trivially_copyable_  = std::is_trivially_copyable_v<T>;
+
+    template< class T >
+    constexpr Truth is_const_               = std::is_const_v<std::remove_reference_t<T>>;
+
+    // Note: scalars include member pointers. So, not a useful category :(
+    //template< class T >
+    //constexpr Truth is_scalar_              = std::is_scalar_v<T>;
 
     //----------------------------------------------------------------------------------------------
 
@@ -94,11 +105,16 @@ namespace cppx
         is_same_type_<Char, signed char>;
 
     template< class Char >
-    constexpr Truth is_a_char_type_ =
-        is_a_byte_char_type_<Char> or
+    constexpr Truth is_a_nonbyte_char_type_ =
         is_same_type_<Char, wchar_t> or
         is_same_type_<Char, char16_t> or
         is_same_type_<Char, char32_t>;
+
+    template< class Char >
+    constexpr Truth is_a_char_type_ =
+        is_a_byte_char_type_<Char> or
+        is_a_nonbyte_char_type_<Char>;
+
         // For C++20 maybe add `char8_t` Ada-ish monstrosity to the list.
 
 }  // namespace cppx
