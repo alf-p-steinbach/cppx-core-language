3ab0441db2e0602376aeda4fa69c73c9e723d2f7
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-05 01:53:31 +0100

    Update number-type-properties.hpp

--- source/cppx-core-language/calc/number-type-properties.hpp
+++ source/cppx-core-language/calc/number-type-properties.hpp
@@ -31,7 +31,7 @@ namespace cppx
 
     namespace impl
     {
-        template< class T > using Nl_ = std::numeric_limits<T>;
+        template< class T > using Info_ = std::numeric_limits<T>;
 
         template< class T, bool is_integral_type = std::is_integral_v<T> >  // "bool" until C++20.
         struct Number_properties_;
@@ -42,10 +42,10 @@ namespace cppx
             static_assert( std::is_integral_v<T> );
 
             static constexpr T      smallest        = 1;                // Smallest non-zero.
-            static constexpr T      largest         = Nl_<T>::max();
-            static constexpr T      min             = Nl_<T>::min();    // Usually `-largest_ - 1`.
+            static constexpr T      largest         = Info_<T>::max();
+            static constexpr T      min             = Info_<T>::min();  // Usually `-largest_ - 1`.
             static constexpr T      max             = largest;
-            static constexpr int    n_digits        = Nl_<T>::digits10;
+            static constexpr int    n_digits        = Info_<T>::digits10;
         };
 
         template< class T >
@@ -53,16 +53,17 @@ namespace cppx
         {
             static_assert( not std::is_integral_v<T> );
 
-            static constexpr Truth  is_ieee_754     = Nl_<T>::is_iec559;
-            static constexpr T      largest         = Nl_<T>::max();
-            static constexpr T      smallest        = Nl_<T>::min();    // Smallest non-zero.
-            static constexpr T      max             = largest;
+            static constexpr T      smallest        = Info_<T>::min();  // Smallest non-zero.
+            static constexpr T      largest         = Info_<T>::max();
             static constexpr T      min             = -largest;         // Largest negative.
-            static constexpr int    max_exp         = Nl_<T>::max_exponent10;
-            static constexpr int    min_exp         = Nl_<T>::min_exponent10;
-            static constexpr int    n_digits        = Nl_<T>::digits10;
-            static constexpr int    radix           = Nl_<T>::radix;
-            static constexpr T      epsilon         = Nl_<T>::epsilon();
+            static constexpr T      max             = largest;
+            static constexpr int    n_digits        = Info_<T>::digits10;
+
+            static constexpr Truth  is_ieee_754     = Info_<T>::is_iec559;
+            static constexpr int    min_exp         = Info_<T>::min_exponent10;
+            static constexpr int    max_exp         = Info_<T>::max_exponent10;
+            static constexpr int    radix           = Info_<T>::radix;
+            static constexpr T      epsilon         = Info_<T>::epsilon();
         };
 
         //TODO: good isnan()
@@ -79,6 +80,40 @@ namespace cppx
     /// @endcond
 
     /// \hideinitializer
+    /// \brief The smallest non-zero absolute value of the type.
+    ///
+    /// For integer `T` this is just 1, and for floating point `T` the expression
+    /// `smallest_<T>` is an alias for `std::numeric_limits<T>::min()`.
+        template< class T > constexpr T     smallest_       = Number_properties_<T>::smallest;
+
+    /// \hideinitializer
+    /// \brief The largest representable-as-positive absolute value of the type.
+    ///
+    /// `largest_<T>` is an alias for `std::numeric_limits<T>::max()`.
+    template< class T > constexpr T     largest_        = Number_properties_<T>::largest;
+
+    /// \hideinitializer
+    /// \brief The largest (yes, the largest) negative value of the type.
+    ///
+    /// For integer `T` the expression `min_<T>` is an alias for
+    /// `std::numeric_limits<T>::min()`, which is usually/always `-largest_<T> - 1`.
+    ///
+    /// For floating point `T` the expression `min_<T>` is an alias for `-largest_<T>`.
+    template< class T > constexpr T     min_            = Number_properties_<T>::min;
+
+    /// \hideinitializer
+    /// \brief The largest positive value of the type.
+    ///
+    /// The expression `max_<T>` is an alias for `largest_<T>`.
+    template< class T > constexpr T     max_            = Number_properties_<T>::max;
+
+    /// \hideinitializer
+    /// \brief Number of decimal digits that guaranteed can be represented.
+    ///
+    /// The expression `n_digits_<T>` is an alias for `std::numeric_limits<T>::digits10`.
+    template< class T > constexpr int   n_digits_       = Number_properties_<T>::n_digits;
+
+    /// \hideinitializer
     /// \brief FP only. Whether the implementation claims to conform to the IEEE 754 standard.
     ///    
     /// `is_ieee_754_<T>` is an alias for `std::numeric_limits<T>::is_iec559`.
@@ -91,41 +126,6 @@ namespace cppx
     template< class T > constexpr Truth is_ieee_754_    = Number_properties_<T>::is_ieee_754;
 
     /// \hideinitializer
-    /// \brief The largest representable-as-positive absolute value of the type.
-    ///
-    /// `largest_<T>` is an alias for `std::numeric_limits<T>::max()`.
-    template< class T > constexpr T     largest_        = Number_properties_<T>::largest;
-
-    /// \hideinitializer
-    /// \brief The smallest non-zero absolute value of the type.
-    ///
-    /// For integer `T` this is just 1, and for floating point `T` the expression
-    /// `smallest_<T>` is an alias for `std::numeric_limits<T>::min()`.
-    template< class T > constexpr T     smallest_       = Number_properties_<T>::smallest;
-
-    /// \hideinitializer
-    /// \brief The largest positive value of the type.
-    ///
-    /// The expression `max_<T>` is an alias for `largest_<T>`.
-    template< class T > constexpr T     max_            = Number_properties_<T>::max;
-
-    /// \hideinitializer
-    /// \brief The largest negative value of the type.
-    ///
-    /// For integer `T` the expression `min_<T>` is an alias for
-    /// `std::numeric_limits<T>::min()`, which is usually/always `-largest_<T> - 1`.
-    ///
-    /// For floating point `T` the expression `min_<T>` is an alias for `-largest_<T>`.
-    template< class T > constexpr T     min_            = Number_properties_<T>::min;
-
-    /// \hideinitializer
-    /// \brief FP only. E-notation maximum (largest positive) exponent value.
-    ///
-    /// The expression `max_exp_<T>` is an alias for
-    /// `std::numeric_limits<T>::max_exponent10`.
-    template< class T > constexpr int   max_exp_        = Number_properties_<T>::max_exp;
-
-    /// \hideinitializer
     /// \brief FP only. E-notation minimum (largest negative) exponent value.
     ///
     /// The expression `min_exp_<T>` is an alias for
@@ -133,10 +133,11 @@ namespace cppx
     template< class T > constexpr int   min_exp_        = Number_properties_<T>::min_exp;
 
     /// \hideinitializer
-    /// \brief Number of decimal digits that guaranteed can be represented.
+    /// \brief FP only. E-notation maximum (largest positive) exponent value.
     ///
-    /// The expression `n_digits_<T>` is an alias for `std::numeric_limits<T>::digits10`.
-    template< class T > constexpr int   n_digits_       = Number_properties_<T>::n_digits;
+    /// The expression `max_exp_<T>` is an alias for
+    /// `std::numeric_limits<T>::max_exponent10`.
+    template< class T > constexpr int   max_exp_        = Number_properties_<T>::max_exp;
 
      /// \hideinitializer
     /// \brief FP only. The numeral system base of the floating point representation,
@@ -157,21 +158,21 @@ namespace cppx
     template< class T > constexpr T     epsilon_        = Number_properties_<T>::epsilon;
 
     /// \brief Provides
-    /// \make_name_ref{cppx,is_ieee_754_},
-    /// \make_name_ref{cppx,largest_},
     /// \make_name_ref{cppx,smallest_},
-    /// \make_name_ref{cppx,max_},
+    /// \make_name_ref{cppx,largest_},
     /// \make_name_ref{cppx,min_},
-    /// \make_name_ref{cppx,max_e_},
-    /// \make_name_ref{cppx,min_e_},
+    /// \make_name_ref{cppx,max_},
     /// \make_name_ref{cppx,n_digits_},
+    /// \make_name_ref{cppx,is_ieee_754_},
+    /// \make_name_ref{cppx,min_e_},
+    /// \make_name_ref{cppx,max_e_},
     /// \make_name_ref{cppx,radix_} and
     /// \make_name_ref{cppx,epsilon_}.
     namespace calc
     {
         CPPX_USE_CPPX(
-            is_ieee_754_, largest_, smallest_, max_, min_, max_exp_, min_exp_, n_digits_,
-            radix_, epsilon_
+            smallest_, largest_, min_, max_, n_digits_,
+            is_ieee_754_, min_exp_, max_exp_, radix_, epsilon_
             );
     }  // namespace calc
 
