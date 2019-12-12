d538b2321a6460a7b37db0f3f42929feea15f45e
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-12 11:09:13 +0100

    in mid-work

--- source/cppx-core-language/syntax/flow-control/all.hpp
+++ source/cppx-core-language/syntax/flow-control/all.hpp
@@ -2,3 +2,7 @@
 #include <cppx-core-language/syntax/flow-control/macro-repeat_times.hpp>
 #include <cppx-core-language/syntax/flow-control/noreturn.hpp>
 #include <cppx-core-language/syntax/flow-control/repeat_times.hpp>
+
+// Convenience includes to support range based `for` loops.
+#include <cppx-core-language/syntax/types/Sequence_.hpp>
+#include <cppx-core-language/syntax/types/Span_.hpp>
