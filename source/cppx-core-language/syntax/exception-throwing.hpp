b196fbdf4e2cfb953e64623974fa01afde1ac52a
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-12 06:18:48 +0100

    Update exception-throwing.hpp

--- source/cppx-core-language/syntax/exception-throwing.hpp
+++ source/cppx-core-language/syntax/exception-throwing.hpp
@@ -1,4 +1,4 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 
 #include <cppx-core-language/syntax/exception-throwing/all.hpp>
-#include <cppx-core-language/syntax/noreturn.hpp>           // cppx::noreturn       // Convenience
+#include <cppx-core-language/syntax/noreturn.hpp>           // Convenience
