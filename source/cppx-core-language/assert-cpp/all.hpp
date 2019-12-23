e87dfb8bef002d0324bf266414a7e5cd6c309996
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-23 01:42:14 +0100

    Create all.hpp

---
+++ source/cppx-core-language/assert-cpp/all.hpp
@@ -0,0 +1,4 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+
+#include <cppx-core-language/assert-cpp/has-utf8-execution-character-set.hpp>
+#include <cppx-core-language/assert-cpp/is-c++17-or-later.hpp>
