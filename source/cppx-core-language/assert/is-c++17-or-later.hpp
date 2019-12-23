0bcfb44bd8d8970fb36ecaf2474cc05ec13fc7c5
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-23 01:18:06 +0100

    -

--- source/cppx-core-language/assert/is-c++17-or-later.hpp
+++ source/cppx-core-language/assert/is-c++17-or-later.hpp
@@ -1,7 +1,7 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 #if __cplusplus < 201703L
     static_assert( __cplusplus >= 201703L,
-        "Requires C++17 or later. For MSVC use e.g. `/std:c++17 /Zc:__cplusplus /utf-8 /wd4459`."
+        "Requires C++17 or later. For MSVC use e.g. `/std:c++17 /Zc:__cplusplus /FIiso646.h /wd4459 /utf-8`."
         // https://blogs.msdn.microsoft.com/vcblog/2018/04/09/msvc-now-correctly-reports-__cplusplus/
         );
 #   include <C++17-or-later-is-required>
