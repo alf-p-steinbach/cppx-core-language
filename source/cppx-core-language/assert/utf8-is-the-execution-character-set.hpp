8b666bd10420a93ab3d56cb05d97d92b848c8b4d
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-23 01:04:26 +0100

    Moved assertions here from cpp-header-collections project

---
+++ source/cppx-core-language/assert/utf8-is-the-execution-character-set.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once    // Source encoding: UTF_8 with BOM (π is a lowercase Greek "pi").
+
+namespace uuid_344a6b2a_64d6_4152_9e71_de4e73ab8b1a {
+    constexpr inline auto utf8_is_the_execution_character_set()
+        -> bool
+    {
+        constexpr auto& slashed_o = "ø";
+        return (sizeof( slashed_o ) == 3 and slashed_o[0] == '\xC3' and slashed_o[1] == '\xB8');
+    }
+
+    static_assert(
+        utf8_is_the_execution_character_set(),
+        "The execution character set must be UTF_8 (e.g. MSVC option \"/utf_8\")."
+        );
+}  // namespace uuid_344a6b2a_64d6_4152_9e71_de4e73ab8b1a
